fe606319f4508ef279008b9deb75cc15cc697dbd
ronald-it <ronald.cirgofer@hotmail.com>
2022-07-21 17:24:38 +0200

    Code klein beetje schoongemaakt

--- src/functions/handleClickCalculator.js
+++ src/functions/handleClickCalculator.js
@@ -1,11 +1,15 @@
+// Import statements
+
 import fetchDataCalculatorSearch from "./fetchDataCalculatorSearch";
 import fetchDataCalculatorAdd from "./fetchDataCalculatorAdd";
 import fetchDataCalculatorCalories from "./fetchDataCalculatorCalories";
+
 // Elementen voor de search button event listener
+
 const submitButton = document.getElementById('submit-form-calculator-search')
 const productInput = document.getElementById('search-bar-calculator')
 let productArray = [];
-let caloriesSum = 0;
+
 // Submit event voor handleClickCalculator
 
 submitButton.addEventListener("submit", (e) => {
@@ -15,22 +19,21 @@ submitButton.addEventListener("submit", (e) => {
     productArray.push(productInput.value);
     console.log(productArray);
 })
+
 // Elementen voor de add button event listener
+
 const addButton = document.getElementById('submit-form-calculator-add');
+
 // Add event voor handleClickCalculator
 
 addButton.addEventListener("submit", (e) => {
+
     e.preventDefault();
+
     console.log(e.target.value)
+
     const tbody = document.getElementById('tbody-add-table');
-    // tbody.replaceChildren();
-    // const rowOneCalculation = document.createElement('tr');
-    // rowOneCalculation.setAttribute('class', 'row-one-calculation');
-    // rowOneCalculation.innerHTML = `                                                <td>Product</td>
-    //                 <td>Calories</td>
-    //                 <td>Fat</td>
-    //                 <td>Carbs</td>`
-    // tbody.appendChild(rowOneCalculation);
+
     for (let i = 0; i < productArray.length; i++) {
         fetchDataCalculatorAdd(productArray[i]);
     }
